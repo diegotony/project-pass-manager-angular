cd30080b9af700e95c0ddd8e27ed779dda8b9624
Alex Fernando León <axfernandoleon@outlook.com>
2019-11-03 19:55:46 -0500

    almacenando autenticacion de usuario

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -5,20 +5,25 @@ import { actionCodeSettings } from "../../../environments/environment";
 import { AngularFireAuth } from '@angular/fire/auth';
 import { map } from 'rxjs-compat/operator/map';
 import { auth } from 'firebase/app';
+import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) { }
 
   loginGoogle() {
-    return this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    return this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+    .then((credential)=> {
+      console.log(credential.user);
+      this.updateUserData(credential.user);
+    })
   }
 
   loginLinkEmail(){
-    
+
   }
 
   isAuth() {
@@ -29,6 +34,13 @@ export class AuthService {
     return this.afAuth.auth.signOut();
   }
 
-
+  private updateUserData(user) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const data = {
+      id: user.uid,
+      email: user.email
+    }
+    return userRef.set(data, { merge: true })
+  }
 
 }
